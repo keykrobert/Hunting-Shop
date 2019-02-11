6c7ae74182dbe5f969f6f924d6d15097c14ea3ac
keykrobert <keykrobert2@gmail.com>
2019-02-11 18:02:17 +0200

    add resources for weapons & update views(show) & added activeadmim

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 #for registration
 gem 'devise'
+#odmenka
+gem 'activeadmin'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'weapons/new'
-  get 'weapons/edit'
-  get 'weapons/show'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+ 
   devise_for :users
 
   root 'main_pages#index'
+  resources :weapons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
