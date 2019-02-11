d2cdf97649c4cbc6d43457ac32b6492e57e626c2
keykrobert <keykrobert2@gmail.com>
2019-02-11 16:59:59 +0200

    Added devise

---
+++ test/controllers/main_pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class MainPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get main_pages_index_url
+    assert_response :success
+  end
+
+end
